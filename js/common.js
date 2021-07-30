620a105a583c0383a5cf61be17dcafe3129845c1
yunjoa <52683681+yunjoa@users.noreply.github.com>
2021-07-31 01:09:47 +0900

    아~~~~
    다시는 미루지 말자..☆

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ $(function(){
     $('footer').load('inc.html footer > div');
 
     function navFun(){
-        $('.gnb li').on({
+        $('header').on({
             mouseenter:function(){
                 $('.dep1').stop().slideDown(200);
                 $('header').addClass('active');
@@ -30,6 +30,8 @@ $(function(){
     }
 
 
+
+    // 네비 스크롤시 하이드 쇼 
     (function(){
 
         var doc = document.documentElement;
@@ -77,4 +79,50 @@ $(function(){
     })();
 
 
+
+    
+    // 엘리먼트 스크롤시 위로 올라오기
+
+    function isElementUnderBottom(elem, triggerDiff) {
+        const { top } = elem.getBoundingClientRect();
+        const { innerHeight } = window;
+        return top > innerHeight + (triggerDiff || 0);
+    }
+
+    function isElementRightToLeft(elem2, triggerDiff2) {
+        const { left } = elem2.getBoundingClientRect();
+        const { innerWidth } = window;
+        return left > innerWidth + (triggerDiff2 || 0);
+    }
+    
+    function handleScroll() {
+        const elems = document.querySelectorAll('.up-on-scroll');
+        elems.forEach(elem => {
+        if (isElementUnderBottom(elem, -20)) {
+            elem.style.opacity = "0";
+            elem.style.transform = 'translateY(70px)';
+        } else {
+            elem.style.opacity = "1";
+            elem.style.transform = 'translateY(0px)';
+        }
+        })
+    }
+
+    function handleScroll2() {
+        const elems2 = document.querySelectorAll('.right-left-scroll');
+        elems2.forEach(elem => {
+        if (isElementRightToLeft(elem2, -20)) {
+            elem2.style.opacity = "0";
+            elem2.style.transform = 'translateX(70px)';
+        } else {
+            elem2.style.opacity = "1";
+            elem2.style.transform = 'translateX(0px)';
+        }
+        })
+    }
+
+
+    
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll2);
 });
