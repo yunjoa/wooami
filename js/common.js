3e17516197a6c5fa45acfc06e22d05769f81218c
jihwa <mungsome@gmail.com>
2022-06-30 11:36:41 +0900

    cookie modify

--- js/common.js
+++ js/common.js
@@ -1,128 +1,115 @@
-$(function(){
-    $('header').load('inc.html header > div',navFun);
-    $('footer').load('inc.html footer > div');
+$(function () {
+  $("header").load("inc.html header > div", navFun);
+  $("footer").load("inc.html footer > div");
 
-    function navFun(){
-        $('header').on({
-            mouseenter:function(){
-                $('.dep1').stop().slideDown(200);
-                $('header').addClass('active');
-            },
-            mouseleave:function(){
-                $('.dep1').stop().slideUp(200);
-                $('header').removeClass('active');
-            }
-        });
-        $('.search_icon').on({
-            click:function(e){
-            e.preventDefault();
-            $('.search').show();
-            $('.search_cancle_icon').show();
-            }
-        });
-        $('.search_cancle_icon').on({
-            click:function(){
-            $('.search').hide();
-            $('.search_cancle_icon').hide();
-            }
-        });
+  function navFun() {
+    $("header").on({
+      mouseenter: function () {
+        $(".dep1").stop().slideDown(200);
+        $("header").addClass("active");
+      },
+      mouseleave: function () {
+        $(".dep1").stop().slideUp(200);
+        $("header").removeClass("active");
+      },
+    });
+    $(".search_icon").on({
+      click: function (e) {
+        e.preventDefault();
+        $(".search").show();
+        $(".search_cancle_icon").show();
+      },
+    });
+    $(".search_cancle_icon").on({
+      click: function () {
+        $(".search").hide();
+        $(".search_cancle_icon").hide();
+      },
+    });
+  }
 
-    }
+  // 네비 스크롤시 하이드 쇼
+  (function () {
+    var doc = document.documentElement;
+    var w = window;
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curScroll;
+    var direction = 0;
+    var prevDirection = 0;
+    var header = document.querySelector("header");
+    var checkScroll = function () {
+      /*
+       ** Find the direction of scroll
+       ** 0 - initial, 1 - up, 2 - down
+       */
+      curScroll = w.scrollY || doc.scrollTop;
+      if (curScroll > prevScroll) {
+        //scrolled up
+        direction = 2;
+      } else if (curScroll < prevScroll) {
+        //scrolled down
+        direction = 1;
+      }
 
+      if (direction !== prevDirection) {
+        toggleHeader(direction, curScroll);
+      }
+      prevScroll = curScroll;
+    };
 
+    var toggleHeader = function (direction, curScroll) {
+      if (direction === 2 && curScroll > 75) {
+        header.classList.add("hide");
+        prevDirection = direction;
+      } else if (direction === 1) {
+        header.classList.remove("hide");
+        prevDirection = direction;
+      }
+    };
 
-    // 네비 스크롤시 하이드 쇼 
-    (function(){
+    window.addEventListener("scroll", checkScroll);
+  })();
 
-        var doc = document.documentElement;
-        var w = window;
-        var prevScroll = w.scrollY || doc.scrollTop;
-        var curScroll;
-        var direction = 0;
-        var prevDirection = 0;
-        var header = document.querySelector('header');
-        var checkScroll = function() {
-        
-                /*
-            ** Find the direction of scroll
-            ** 0 - initial, 1 - up, 2 - down
-            */
-            curScroll = w.scrollY || doc.scrollTop;
-            if (curScroll > prevScroll) { 
-                //scrolled up
-                direction = 2;
-            } else if (curScroll < prevScroll) { 
-                //scrolled down
-                direction = 1;
-            }
+  // 엘리먼트 스크롤시 위로 올라오기
 
-            if (direction !== prevDirection) {
-                toggleHeader(direction, curScroll);
-            }
-            prevScroll = curScroll;
-        };
+  function isElementUnderBottom(elem, triggerDiff) {
+    const { top } = elem.getBoundingClientRect();
+    const { innerHeight } = window;
+    return top > innerHeight + (triggerDiff || 0);
+  }
 
+  function isElementRightToLeft(elem2, triggerDiff2) {
+    const { right } = elem2.getBoundingClientRect();
+    const { innerWidth } = window;
+    return right > innerWidth + (triggerDiff2 || 0);
+  }
 
-        var toggleHeader = function(direction, curScroll) {
-            if (direction === 2 && curScroll > 75) { 
-                header.classList.add('hide');
-                prevDirection = direction;
+  function handleScroll() {
+    const elems = document.querySelectorAll(".up-on-scroll");
+    elems.forEach((elem) => {
+      if (isElementUnderBottom(elem, -20)) {
+        elem.style.opacity = "0";
+        elem.style.transform = "translateY(70px)";
+      } else {
+        elem.style.opacity = "1";
+        elem.style.transform = "translateY(0px)";
+      }
+    });
+  }
 
-            } else if (direction === 1) {
-                header.classList.remove('hide');
-                prevDirection = direction;
+  function handleScroll2() {
+    const elems2 = document.querySelectorAll(".right-left-scroll");
+    elems2.forEach((elem) => {
+      if (isElementRightToLeft(elem, -20)) {
+        elem.style.opacity = "0";
+        elem.style.transform = "translateX(70px)";
+      } else {
+        elem.style.opacity = "1";
+        elem.style.transform = "translateX(0px)";
+      }
+    });
+  }
 
-            }
-        };
-        
-        window.addEventListener('scroll', checkScroll);
-    })();
-
-
-
-    
-    // 엘리먼트 스크롤시 위로 올라오기
-
-    function isElementUnderBottom(elem, triggerDiff) {
-        const { top } = elem.getBoundingClientRect();
-        const { innerHeight } = window;
-        return top > innerHeight + (triggerDiff || 0);
-    }
-
-    function isElementRightToLeft(elem2, triggerDiff2) {
-        const { right } = elem2.getBoundingClientRect();
-        const { innerWidth } = window;
-        return right > innerWidth + (triggerDiff2 || 0);
-    }
-    
-    function handleScroll() {
-        const elems = document.querySelectorAll('.up-on-scroll');
-        elems.forEach(elem => {
-        if (isElementUnderBottom(elem, -20)) {
-            elem.style.opacity = "0";
-            elem.style.transform = 'translateY(70px)';
-        } else {
-            elem.style.opacity = "1";
-            elem.style.transform = 'translateY(0px)';
-        }
-        })
-    }
-
-    function handleScroll2() {
-        const elems2 = document.querySelectorAll('.right-left-scroll');
-        elems2.forEach(elem => {
-        if (isElementRightToLeft(elem2, -20)) {
-            elem2.style.opacity = "0";
-            elem2.style.transform = 'translateX(70px)';
-        } else {
-            elem2.style.opacity = "1";
-            elem2.style.transform = 'translateX(0px)';
-        }
-        })
-    }
-
-
-    
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('scroll', handleScroll2);
+  window.addEventListener("scroll", handleScroll);
+  window.addEventListener("scroll", handleScroll2);
 });
--- js/index.js
+++ js/index.js
@@ -1,37 +1,98 @@
-$(function(){
+$(function () {
+  $(".main_slider").slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1000,
+  });
 
-    $(".main_slider").slick({
-        arrows:false,
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeed:3000,
-        speed:1000
-    });
+  $(".sub_slider").slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1000,
+  });
 
+  $(".review_box").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
 
-    $(".sub_slider").slick({
-        arrows: true,
-        dots: false,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeed:3000,
-        speed:1000
-    });
+  let myPopup = document.querySelector(".popup"),
+    checkbox = document.querySelector(".popupCheck"),
+    popupClose = document.querySelector(".btn_close");
 
-    $('.review_box').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeed:1000
+  // 쿠키 셋팅
+  function setCookie(name, value, sec) {
+    let date = new Date(); //현재날짜지정
+    date.setSeconds(date.getSeconds() + sec);
 
-    });
+    let mycookie = "";
+    mycookie += name + "=" + value + ";";
+    mycookie += "Expires=" + date.toUTCString();
 
-    
+    document.cookie = mycookie;
+  }
+  // setCookie('stranger','Main',30)
 
-})
+  //쿠키 확인
+  console.log(document.cookie);
+
+  function checkCookie(name) {
+    let cookies = document.cookie.split(";");
+
+    let visited = false; //방문 거짓
+
+    for (var i in cookies) {
+      if (cookies[i].indexOf(name) > -1) {
+        visited = true;
+      }
+    }
+    if (visited) {
+      //재방문
+      myPopup.style.display = "none";
+    } else {
+      //신규방문
+      myPopup.style.display = "block";
+    }
+  }
+  checkCookie("stranger");
+
+  popupClose.addEventListener("click", function () {
+    //a.checked true / flase
+    if (checkbox.checked) {
+      //팝업을 다시 안보겠다 팝업 닫고 쿠키 생성
+      setCookie("stranger", "Main", 10);
+      myPopup.style.display = "none";
+    } else {
+      //팝업을 계속 본다. 팝업 닫고, 쿠키제거.
+      myPopup.style.display = "none";
+      delCookie("stranger");
+    }
+  });
+
+  //쿠키삭제
+
+  function delCookie(name) {
+    let date = new Date();
+
+    date.setDate(date.getDate() - 1);
+
+    let setCookie = "";
+    setCookie += name + "=Main;";
+    setCookie += "Expires=" + date.toUTCString();
+
+    document.cookie = setCookie;
+  }
+});
